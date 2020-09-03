456d001410d060e3998acf069efa99c486dc3761
AntonBiryukovUofC <anton.biryukov@ucalgary.ca>
2020-09-03 11:45:24 -0600

    simple viz of dataset

---
+++ src/st_drawing.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+from quickdraw import QuickDrawData
+
+qd = QuickDrawData(recognized=True,jit_loading=False,max_drawings=1000)
+qd.load_all_drawings()
+drawings = qd.loaded_drawings
+n_strokes = [d.no_of_strokes for d in drawings]
+
+anvil = qd.get_drawing(name = 'dolphin')
+st.image(anvil.image)
+st.write(f'Groups loaded: {qd.drawing_names}')
--- src/st_drawing_test.py
+++ src/st_drawing_test.py
@@ -1,12 +1,41 @@
 import streamlit as st
 import pandas as pd
 from quickdraw import QuickDrawData
+import matplotlib.pyplot as plt
+import numpy as np
 
-qd = QuickDrawData(recognized=True,jit_loading=False,max_drawings=1000)
-qd.load_all_drawings()
-drawings = qd.loaded_drawings
-n_strokes = [d.no_of_strokes for d in drawings]
+qd = QuickDrawData(jit_loading=True, max_drawings=100)
 
-anvil = qd.get_drawing(name = 'dolphin')
-st.image(anvil.image)
-st.write(f'Groups loaded: {qd.drawing_names}')
+
+def draw_pic(drawing):
+    fig, ax = plt.subplots(figsize=(2, 2))
+    for stroke in drawing.strokes:
+        x = [pt[0] for pt in stroke]
+        y = [pt[1] for pt in stroke]
+        ax.plot(x, y)
+    ax.set_title(drawing.name)
+    return fig, ax
+
+
+def reshape_strokes(drawing):
+    """
+    Return list of dictionaries with strokes
+    """
+    lines = []
+    for i, stroke in enumerate(drawing.strokes):
+        x = ([pt[0] for pt in stroke])
+        y = ([pt[1] for pt in stroke])
+        id_stroke = i
+        length = np.sum(np.sqrt(np.diff(x)**2 + np.diff(y)**2))
+        line = {'id_stroke': id_stroke, 'x': x, 'y': y,'len':length,
+                'n_pts':len(x)}
+        lines.append(line)
+    return lines
+
+
+drawing = qd.get_drawing(name='key')
+st.write(f'strokes : {drawing.no_of_strokes}')
+fig, ax = draw_pic(drawing)
+lines = reshape_strokes(drawing)
+st.pyplot(fig,figsize=(2, 2))
+st.write(lines)
