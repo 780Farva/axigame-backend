6d87e285437634fe6afda111f9c636a94d6afa0d
jules <jlaprairie@gmail.com>
2020-09-03 10:23:53 -0600

    Stubbed state transition handlers.

--- src/game_manager.py
+++ src/game_manager.py
@@ -1,13 +1,24 @@
-
 from transitions import Machine
+import logging
+
+log = logging.getLogger(__name__)
+
 
 class GameManager:
     """Handles the game's states and invokes AxiDraw commands"""
-    states = ['stopped', 'started']
+
+    states = ["stopped", "started"]
+
     def __init__(self):
         # Initialize the state machine
-        self.machine = Machine(model=self, states=GameManager.states, initial='stopped')
+        self.machine = Machine(model=self, states=GameManager.states, initial="stopped")
 
         # Add state transitions
-        self.machine.add_transition(trigger='start', source='stopped', dest='started')
-        self.machine.add_transition(trigger='stop', source='started', dest='stopped')
+        self.machine.add_transition(trigger="start", source="stopped", dest="started")
+        self.machine.add_transition(trigger="stop", source="started", dest="stopped")
+
+    def on_enter_started(self):
+        pass
+
+    def on_enter_stopped(self):
+        pass
