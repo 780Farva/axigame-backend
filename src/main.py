eed5a179accad488695c5d14f41d73752504c4d1
jules <jlaprairie@gmail.com>
2020-09-03 09:56:34 -0600

    Added stub endpoints for startGame, guess, and startCameraFeed

--- src/main.py
+++ src/main.py
@@ -2,6 +2,17 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/')
-async def root():
-    return {"message": "I'm alive"}
+
+@app.post("/startGame")
+async def start_game():
+    return {"message": "TODO: This will start the game."}
+
+
+@app.post("/guess")
+async def start_game():
+    return {"message": "TODO: This will register a guess."}
+
+
+@app.post("/startCameraFeed")
+async def start_game():
+    return {"message": "TODO: This will start the camera feed."}
