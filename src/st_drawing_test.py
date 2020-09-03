f7fef7d02f14db9c7c2d80457ad6cdb8a6350e0a
AntonBiryukovUofC <anton.biryukov@ucalgary.ca>
2020-09-03 13:34:38 -0600

    added drawing

--- src/st_drawing_test.py
+++ src/st_drawing_test.py
@@ -3,6 +3,7 @@ import pandas as pd
 from quickdraw import QuickDrawData
 import matplotlib.pyplot as plt
 import numpy as np
+from pyaxidraw.axidraw import AxiDraw
 
 qd = QuickDrawData(jit_loading=True, max_drawings=100)
 
@@ -17,25 +18,56 @@ def draw_pic(drawing):
     return fig, ax
 
 
-def reshape_strokes(drawing):
+def reshape_strokes(drawing, scale=5):
     """
     Return list of dictionaries with strokes
     """
     lines = []
     for i, stroke in enumerate(drawing.strokes):
-        x = ([pt[0] for pt in stroke])
-        y = ([pt[1] for pt in stroke])
+        x = np.array([pt[0] / scale for pt in stroke])
+        y = np.array([pt[1] / scale for pt in stroke])
         id_stroke = i
-        length = np.sum(np.sqrt(np.diff(x)**2 + np.diff(y)**2))
-        line = {'id_stroke': id_stroke, 'x': x, 'y': y,'len':length,
-                'n_pts':len(x)}
+        length = np.sum(np.sqrt(np.diff(x) ** 2 + np.diff(y) ** 2))
+        line = {'id_stroke': id_stroke, 'x': x, 'y': y, 'len': length,
+                'n_pts': len(x)}
         lines.append(line)
     return lines
 
 
-drawing = qd.get_drawing(name='key')
-st.write(f'strokes : {drawing.no_of_strokes}')
-fig, ax = draw_pic(drawing)
-lines = reshape_strokes(drawing)
-st.pyplot(fig,figsize=(2, 2))
-st.write(lines)
+def draw_one_line(ad, l):
+    start = l['x'][0], l['y'][0]
+    # Move to first point in stroke
+    ad.move(*start)
+    for x, y in zip(np.diff(l['x']), np.diff(l['y'])):
+        ad.line(x, y)
+
+
+def draw_lines(ad: AxiDraw, lines, reference_xy=(0, 0)):
+    # Goto ref
+    for l in lines:
+        ad.moveto(reference_xy[0], reference_xy[1])
+        draw_one_line(ad, l)
+
+
+ad = AxiDraw()
+ad.interactive()
+ad.connect()
+ad.options.speed_pendown=1
+ad.options.speed_penup=100
+ad.options.units = 2
+ad.update()
+try:
+    # 6435186029887488
+    drawing = qd.get_drawing(name='key',index = 1)
+
+    st.write(f'strokes : {drawing.no_of_strokes}')
+    fig, ax = draw_pic(drawing)
+    fig.savefig('test.png')
+    lines = reshape_strokes(drawing,scale=5)
+    draw_lines(ad,lines,reference_xy=(102,102))
+#     st.pyplot(fig,figsize=(2, 2))
+#    st.write(lines)
+
+finally:
+    ad.moveto(0, 0)
+    ad.disconnect()
