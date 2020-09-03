698e9b1133adcb9b155273efdcb04dae077e61b3
jules <jlaprairie@gmail.com>
2020-09-03 10:34:24 -0600

    Stubbed out a couple axidraw commands as a canary check

--- src/game_manager.py
+++ src/game_manager.py
@@ -1,6 +1,7 @@
 from transitions import Machine
 import logging
 from quickdraw import QuickDrawData
+from pyaxidraw.axidraw import AxiDraw
 
 log = logging.getLogger(__name__)
 
@@ -17,10 +18,26 @@ class GameManager:
         # Add state transitions
         self._machine.add_transition(trigger="start", source="stopped", dest="started")
         self._machine.add_transition(trigger="stop", source="started", dest="stopped")
+
+        # Init the quickdraw api client
         self._qd = QuickDrawData()
 
+        # Init the axidraw client
+        self._ad = AxiDraw()
+
     def on_enter_started(self):
+        # Go grab a hamburger
+        # TODO: Make this chose a random topic from a group of valid topics
         self._qd.get_drawing("hamburger")
 
+        # Show some sign of life on the axidraw
+        # TODO: Make this start drawing out the hamburger or what have you.
+        self._ad.interactive()
+        self._ad.connect()
+        self._ad.moveto(1,1)
+
     def on_enter_stopped(self):
-        pass
+        # TODO: Clean up, lift up pen, etc
+        # Move back to home
+        self._ad.moveto(0,0)
+        self._ad.disconnect()
